08d5e108fba721187f705613e92d51c72a77b13a
vasuchal <vasudhachalasani@gmail.com>
2024-02-17 03:31:40 -0800

    Add custom styles from Figma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,29 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'h1-size': '96px', // Existing custom font size
+        'h2-size': '64px', // New custom font size
+        'h3-size': '48px',
+        'h4-size': '36px',
+        'text-size':'16px'
+      },
+      fontWeight: {
+        'extrabold': '900', // Ensure extrabold is correctly mapped if not already
+        'semibold': '600',
+        "medium": '400'
+      },
+      colors: {
+        darkBlue: "#1A3F67",
+        mediumBlue: "4A729D",
+        midBlue: "88ADD5",
+        lightBlue: "BEDDFC",
+        black: "000000",
+        white: '#FFFFFF',
+        lightGray: "EBEBEB",
+        darkGray: "BDBDBD"
       },
     },
   },
