2774ea57f2ad101ae490a0e0caa47738c65d200f
vasuchal <vasudhachalasani@gmail.com>
2024-02-17 12:04:50 -0800

    Set up teacher template

---
+++ src/server/api/routers/teacher.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const classroomRouter = createTRPCRouter({
+  // TODO: GET teacher
+  getTeacher: publicProcedure.input(z.object({})).query(({ ctx }) => {
+    const sets = ctx.db.classroom.findMany();
+  }),
+
+  // TODO: Add to teacher
+  addClassroomToTeacher: publicProcedure
+    .input(z.object({}))
+    .query(({ ctx }) => {
+      const sets = ctx.db.classroom.findMany();
+    }),
+});
