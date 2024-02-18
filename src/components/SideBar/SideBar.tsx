74b977c3f5a7b1cc0067c0db663b9f410970c3e3
Archna-1 <archnas2@illinois.edu>
2024-02-17 19:37:35 -0800

    Fix sidebar logo

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -4,7 +4,10 @@ import {
   useRadioGroup,
   Heading,
   Box,
+  HStack,
 } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 import React, { type Dispatch, type SetStateAction } from "react";
 import ClassRadioButton from "./ClassRadioButton";
@@ -36,9 +39,14 @@ const Sidebar = ({
       top="0"
       zIndex="sticky"
     >
-      <Heading textAlign="center" mt={3} className="main-class text-white">
-        ActiveCard
-      </Heading>
+      <HStack px={2} gap={0}>
+        <div className="py-5 pl-2 hover:opacity-75">
+          <Link href="/">
+            <Image src="/assets/logo.png" alt="header" width={65} height={65} />
+          </Link>
+        </div>
+        <Heading className="main-class text-white">ActiveCard</Heading>
+      </HStack>
       <VStack spacing={0}>
         {classes.map((c) => {
           const radio = getRadioProps({ value: c.name });
