7757e73bcd17cf991b480e02e31c2f1059a2b024
zorazrr <zorazrr@gmail.com>
2024-02-17 19:53:14 -0800

    Add toast

--- src/pages/set/[id].tsx
+++ src/pages/set/[id].tsx
@@ -2,33 +2,50 @@ import { useState } from 'react'; // Import useState hook
 import { useRouter } from 'next/router';
 import FlashCard from '~/components/Flashcard';
 import { api } from '~/utils/api';
-import { Icon } from '@chakra-ui/react';
+import { Icon, useToast } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 const Set = () => {
     const setId = useRouter().query.id;
     const { data: cards } = api.card.geCardBySet.useQuery({ setId: setId as string },
         { enabled: !!setId, retry: false, refetchOnWindowFocus: false });
-
-    // State to keep track of the current index
     const [curIndex, setCurIndex] = useState(0);
+    const toast = useToast();
 
     if (!cards) {
         return <div>Loading...</div>;
     }
 
-    // Check if cards array is not empty and curIndex is within bounds
-    const isValidIndex = curIndex >= 0 && curIndex < cards.length;
+    const handleCorrectAnswer = () => {
+        setCurIndex(curIndex + 1);
+        toast({
+            title: "That is correct!",
+            status: "success",
+            duration: 3000,
+            isClosable: false,
+            position: "top"
+        });
+    }
+
+    const handleIncorrectAnswer = () => {
+        toast({
+            title: "Try again!",
+            status: "error",
+            duration: 3000,
+            isClosable: false,
+            position: "top"
+        });
+    }
 
     return (
         <div className="flex flex-col h-screen w-screen justify-start items-start">
             {
-                isValidIndex ? (
+                curIndex >= 0 && curIndex < cards.length ? (
                     <FlashCard
                         key={cards[curIndex]!.id}
                         card={cards[curIndex]!}
-                        onCorrectCallback={() => { setCurIndex(curIndex + 1) }}
-                        onIncorrectCallback={() => { console.log("wrong :(") }} />
+                        onCorrectCallback={() => { handleCorrectAnswer() }}
+                        onIncorrectCallback={() => { handleIncorrectAnswer() }} />
                 ) : (
                     <div>No card available</div>
                 )
