a0c8f3537abfa383c6c6969973d21dba2da9d2ee
vasuchal <vasudhachalasani@gmail.com>
2024-02-28 17:51:27 -0600

    Add more comprehensive error code

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,30 +6,32 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { getRoleEnum } from "~/utils/helpers";
 
 export const userRouter = createTRPCRouter({
-    setUserRole: publicProcedure.input(z.object({ userId: z.string(), role: z.string() })).mutation(async ({ ctx, input }) => {
-        const user = await ctx.db.user.findUnique({
-            where: {
-                id: input.userId,
-            },
+  setUserRole: publicProcedure
+    .input(z.object({ userId: z.string(), role: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: input.userId,
+        },
+      });
+
+      console.log(getRoleEnum(input.role));
+
+      if (!user || user.role !== Role.UNKNOWN) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found or user already assigned role",
         });
+      }
 
-        console.log(getRoleEnum(input.role));
-
-        if (!user || user.role !== Role.UNKNOWN) {
-            throw new TRPCError({
-                code: "NOT_FOUND",
-                message: "User not found",
-            });
-        }
-
-        const updatedUser = await ctx.db.user.update({
-            where: {
-                id: input.userId,
-            },
-            data: {
-                role: getRoleEnum(input.role),
-            },
-        });
-        return updatedUser;
+      const updatedUser = await ctx.db.user.update({
+        where: {
+          id: input.userId,
+        },
+        data: {
+          role: getRoleEnum(input.role),
+        },
+      });
+      return updatedUser;
     }),
 });
