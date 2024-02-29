e0351df6bfc1dba94b3c2dcb76c01e34cf473c63
vasuchal <vasudhachalasani@gmail.com>
2024-02-28 21:49:57 -0600

    Refactor code to properly update session state and lead to dashboard, handle edge case login w/o register

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,30 +1,117 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { api } from "~/utils/api";
+import {
+  Spinner,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+import StyledButton from "~/components/Button";
+import { Role } from "@prisma/client";
 
 const Redirect = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
   const userMutation = api.user.setUserRole.useMutation();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleRoleSelection = async (role: string) => {
+    userMutation.mutate(
+      {
+        userId: session!.user?.id,
+        role: role,
+      },
+      {
+        onSuccess: async () => {
+          await signIn("google", {
+            callbackUrl: "/dashboard",
+            redirect: false,
+          });
+        },
+      },
+    );
+    onClose();
+  };
+
   useEffect(() => {
     console.log(router.query.role);
     if (!router.query.role && !session?.user.id) return;
     if (status === "loading") return;
     if (!session?.user.id) return;
+
     userMutation.mutate(
-      { userId: session?.user?.id, role: router.query.role as string },
       {
-        onSuccess: () => {
-          const redirect = async () => await router.push("/dashboard");
-          void redirect();
+        userId: session?.user?.id,
+        role: router.query.role
+          ? (router.query.role as string)
+          : session.user.role,
+      },
+      {
+        onSuccess: async (response) => {
+          console.log(response);
+          if (response.role == Role.UNKNOWN) {
+            onOpen();
+            return;
+          } else {
+            await signIn("google", {
+              callbackUrl: "/dashboard",
+              redirect: false,
+            });
+          }
         },
       },
     );
   }, [router, session?.user.id, status]);
 
-  return <h1>Redirecting...</h1>;
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Spinner thickness="4px" size="xl" />
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size={"xl"}>
+        <ModalOverlay />
+        <ModalContent backgroundColor={"white"}>
+          <ModalHeader>Choose Your Role</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack>
+              <StyledButton
+                label="I'm a Teacher"
+                colorInd={1}
+                onClick={() => {
+                  handleRoleSelection("teacher");
+                }}
+                style={{
+                  paddingTop: "15px",
+                  paddingBottom: "15px",
+                }}
+              />
+              <StyledButton
+                label="I'm a Student"
+                colorInd={2}
+                onClick={() => {
+                  handleRoleSelection("student");
+                }}
+                style={{
+                  paddingTop: "15px",
+                  paddingBottom: "15px",
+                }}
+              />
+              <br></br>
+            </VStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  );
 };
 
 export default Redirect;
