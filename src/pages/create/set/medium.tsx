e6b45b3cad8c790d5f07204c68f38fd267c1a327
Archna-1 <archnas2@illinois.edu>
2024-03-21 10:34:40 -0500

    Create form outline

--- src/pages/create/set/medium.tsx
+++ src/pages/create/set/medium.tsx
@@ -3,7 +3,25 @@ import StyledButton from "~/components/Button";
 import StyledFileUpload from "~/components/FileUpload";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { HStack, Spinner, VStack, useDisclosure } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  HStack,
+  Radio,
+  RadioGroup,
+  Select,
+  Spinner,
+  VStack,
+  useDisclosure,
+  Text,
+  Input,
+  NumberInput,
+  Button,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInputField,
+  NumberInputStepper,
+} from "@chakra-ui/react";
 import StyledModal from "~/components/Modal";
 import { api } from "~/utils/api";
 import { useState } from "react";
@@ -13,6 +31,7 @@ export default function SetCreationMediumSelection() {
   const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   const [flashcards, setFlashcards] = useState<TermDefPair[]>([]);
   const [subject, setSubject] = useState<string>("");
@@ -60,6 +79,24 @@ export default function SetCreationMediumSelection() {
     });
   };
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const setType = form.get("Type of Set");
+    const pomodoroIntervalLength = form.get(
+      "Number of Cards Between Pomodoro Breaks",
+    );
+    const isReviewEnabled = form.get("Enable Review");
+    console.log({ setType, pomodoroIntervalLength, isReviewEnabled });
+    const body = {};
+    for (const [key, value] of form.entries()) {
+      body[key] = value;
+    }
+    console.log(body);
+    // Do Further input validation and submit the form
+    setPage(2);
+  }
+
   return (
     <>
       <div className="p-5 hover:opacity-75">
@@ -70,50 +107,108 @@ export default function SetCreationMediumSelection() {
       {/* Adjusted div for centering text */}
       <div
         className="flex items-start justify-center"
-        style={{ height: "100vh" }}
+        style={{ height: "100vh", width: "100vw" }}
       >
-        <div className="text-center" style={{ marginTop: "5%" }}>
-          <div className="h2 text-darkBlue">Create a Set</div>
-          <br></br>
-          <VStack spacing={8}>
-            <HStack spacing={8}>
-              <StyledFileUpload
-                classId={router.query.classId as string}
-                setIsLoading={setIsLoading}
-              />
-              <StyledButton
-                label="Generate Using AI"
-                colorInd={1}
-                onClick={onOpen}
-                style={{
-                  width: "120%",
-                  paddingTop: "13px",
-                  paddingBottom: "13px",
-                  marginRight: "5%",
-                }}
-              />
-              <div style={{ marginLeft: "25px " }}>
+        {page === 1 && (
+          <div
+            className="text-center"
+            style={{ marginTop: "5%", width: "60%" }}
+          >
+            <div className="h2 text-darkBlue">Create a Set</div>
+            <br></br>
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={8} w="100%">
+                <FormControl w="100%">
+                  <FormLabel>Type of Set</FormLabel>
+                  <RadioGroup defaultValue="Assignment" w="100%">
+                    <HStack
+                      spacing="auto"
+                      w="100%"
+                      justifyContent="space-between"
+                    >
+                      <Radio value="Assignment">Assignment</Radio>
+                      <Radio value="Inverted Classroom">
+                        Inverted Classroom
+                      </Radio>
+                      <Radio value="Literacy">Literacy</Radio>
+                      <Radio value="Theory">Theory</Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Number of Cards Between Pomodoro Breaks</FormLabel>
+                  <NumberInput defaultValue={5} min={3}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+                {/* <FormControl w="100%">
+                  <FormLabel>Enable Review</FormLabel>
+                  <RadioGroup defaultValue="Yes" w="100%">
+                    <HStack
+                      spacing="auto"
+                      w="100%"
+                      justifyContent="space-between"
+                    >
+                      <Radio value="Yes">Yes</Radio>
+                      <Radio value="No">No</Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl> */}
+                <Button type="submit" w="100%" py={8}>
+                  Next
+                </Button>
+              </VStack>
+            </form>
+          </div>
+        )}
+        {page === 2 && (
+          <div className="text-center" style={{ marginTop: "5%" }}>
+            <div className="h2 text-darkBlue">Create a Set</div>
+            <br></br>
+            <VStack spacing={8}>
+              <HStack spacing={8}>
+                <StyledFileUpload
+                  classId={router.query.classId as string}
+                  setIsLoading={setIsLoading}
+                />
                 <StyledButton
-                  label="Create from Scratch"
+                  label="Generate Using AI"
                   colorInd={1}
-                  onClick={createNewSetFromScratch}
+                  onClick={onOpen}
                   style={{
                     width: "120%",
                     paddingTop: "13px",
                     paddingBottom: "13px",
-                    marginRight: "10%",
+                    marginRight: "5%",
                   }}
                 />
-              </div>
-            </HStack>
-            {isLoading && (
-              <>
-                <Spinner size={"xl"} />
-                <div className="h4 text-darkBlue">Building your deck...</div>
-              </>
-            )}
-          </VStack>
-        </div>
+                <div style={{ marginLeft: "25px " }}>
+                  <StyledButton
+                    label="Create from Scratch"
+                    colorInd={1}
+                    onClick={createNewSetFromScratch}
+                    style={{
+                      width: "120%",
+                      paddingTop: "13px",
+                      paddingBottom: "13px",
+                      marginRight: "10%",
+                    }}
+                  />
+                </div>
+              </HStack>
+              {isLoading && (
+                <>
+                  <Spinner size={"xl"} />
+                  <div className="h4 text-darkBlue">Building your deck...</div>
+                </>
+              )}
+            </VStack>
+          </div>
+        )}
       </div>
       <StyledModal
         isOpen={isOpen}
